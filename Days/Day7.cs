8cb610a2444be71bf5241df0dc6a4b30784520a2
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2023-12-07 13:27:30 -0500

    cleanup day7 it's much simpler than I thought

--- Days/Day7.cs
+++ Days/Day7.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Days
 {
@@ -17,17 +11,12 @@ namespace AdventOfCode.Days
         public override void ConvertData()
         {
             var contents = File.ReadAllLines(FilePath);
-            Regex number = new(@"\d+");
-            Regex word = new(@"\w+");
 
             foreach(var line in contents)
             {
                 var split = line.Split(" ");
 
-                var curr = new Hand();
-                curr.Parse(split[0]);
-                curr.Bid = int.Parse(split[1]);
-                curr.GetCardCountDict();
+                var curr = new Hand(split[0], int.Parse(split[1]));
 
                 Hands.Add(curr);
             }
@@ -38,7 +27,7 @@ namespace AdventOfCode.Days
             A = 14,
             K = 13,
             Q = 12,
-            J = 11, // set back to 11 for part 1
+            J = 11,
             T = 10,
             Two = 2,
             Three = 3,
@@ -63,17 +52,15 @@ namespace AdventOfCode.Days
 
         public class Hand
         {
-            public Card[] Cards = new Card[5];
-            public int Bid;
+            public Card[] Cards { get; set; } = new Card[5];
+            public int Bid { get; set; }
+            public Dictionary<Card, int> CardCounts { get; set; } = new();
 
-            public Hand()
+            public Hand(string hand, int bid)
             {
-                
-            }
-
-            public Hand(string cards)
-            {
-                Parse(cards);
+                Bid = bid;
+                Parse(hand);
+                GetCardCountDict();
             }
 
             public void Parse(string hand)
@@ -99,8 +86,6 @@ namespace AdventOfCode.Days
                 }
             }
 
-            public Dictionary<Card, int> CardCounts = new();
-
             public void GetCardCountDict()
             {
                 var values = (Card[])Enum.GetValues(typeof(Card));
@@ -115,105 +100,49 @@ namespace AdventOfCode.Days
                 }
             }
 
-            public static Rank GetRank(Card[] cards)
+            public Rank GetRank()
             {
-                int[] counts = new int[15];
-                for (int i = 0; i < cards.Length; ++i)
-                {
-                    int index = (int)cards[i] - 1;
-                    counts[index]++;
-                }
-
-                if (counts.Contains(5))
+                if (CardCounts.ContainsValue(5))
                     return Rank.Five;
 
-                if (counts.Contains(4))
+                if (CardCounts.ContainsValue(4))
                     return Rank.Four;
 
-                List<int> list = counts.ToList();
-                int indexOf1 = list.IndexOf(2);
+                int pairCount = CardCounts.Values.Where(x => x == 2).Count();
 
-                if (counts.Contains(3))
+                if (CardCounts.ContainsValue(3))
                 {
-                    if (indexOf1 != -1)
+                    if (pairCount == 1)
                         return Rank.Full;
                     return Rank.Three;
                 }
 
-                if (indexOf1 != -1)
-                {
-                    int indexOf2 = list.IndexOf(2, indexOf1 + 1);
-                    if (indexOf2 != -1)
-                        return Rank.Two;
+                if (pairCount == 2)
+                    return Rank.Two;
 
+                if (pairCount == 1)
                     return Rank.One;
-                }
 
                 return Rank.High;
             }
 
-            public Rank GetRank()
+            public Rank IncreaseRank()
             {
-                return GetRank(Cards);
-            }
-
-            public void SetEveryJTo(List<Card> cards, Card set)
-            {
-                for(int i = 0; i < 5; ++i)
-                {
-                    if (cards[i] == Card.J)
-                        cards[i] = set;
-                }
-            }
-
-            public Rank IncreaseRank(out List<Card> cards)
-            {
-                List<Card> newCards = new(Cards);
                 Rank curr = GetRank();
                 int jcount = CardCounts[Card.J];
-                cards = newCards;
                 if (!Cards.Contains(Card.J))
                 {
-                    cards = newCards;
                     return curr;
                 }
 
                 if(curr == Rank.Five)
                 {
-                    SetEveryJTo(newCards, Card.A);
-                    cards = newCards;
                     return Rank.Five;
                 }
 
                 if(curr == Rank.Four)
                 {
-                    Card oneCount = newCards.First(x => x != Card.J);
-                    Card fourCount = CardCounts.First(x => x.Value == 4).Key;
-
-                    if(fourCount == Card.J)
-                    {
-                        SetEveryJTo(newCards, oneCount);
-                        cards = newCards;
-                        return Rank.Five;
-                    }
-                    else
-                    {
-                        SetEveryJTo(newCards, fourCount);
-                        cards = newCards;
-                        return Rank.Five;
-                    }
-                }
-
-                Card HighestCard(params Card[] cards)
-                {
-                    Card high = cards[0];
-                    for (int i = 1; i < cards.Length; ++i)
-                    {
-                        if (cards[i] > high)
-                            high = cards[i];
-                    }
-
-                    return high;
+                    return Rank.Five;
                 }
 
                 if (curr == Rank.Three)
@@ -222,45 +151,30 @@ namespace AdventOfCode.Days
                     Card firstOther = CardCounts.First(x => x.Value != 3).Key;
                     Card secondOther = CardCounts.Last(x => x.Value != 3).Key;
 
+                    // J J J 1 2 => 1 1 1 1 2
                     if (jcount == 3)
                     {
-                        SetEveryJTo(newCards, HighestCard(firstOther, secondOther));
-                        cards = newCards;
                         return Rank.Four;
                     }
 
+                    // J J 1 1 1
                     if(jcount == 2)
                     {
-                        SetEveryJTo(newCards, threeCount);
-                        cards = newCards;
+                        // this is not even possible it would be a full house
                         return Rank.Five;
                     }
 
+                    // J 1 1 1 3 => 1 1 1 1 3
                     if (jcount == 1)
                     {
-                        SetEveryJTo(newCards, threeCount);
-                        cards = newCards;
                         return Rank.Four;
                     }
                 }
 
+                // J J J 1 1 => 1 1 1 1 1
                 if(curr == Rank.Full)
                 {
-                    Card twoCount = CardCounts.First(x => x.Value == 2).Key;
-                    Card threeCount = CardCounts.First(x => x.Value == 3).Key;
-
-                    if(threeCount == Card.J)
-                    {
-                        SetEveryJTo(newCards, twoCount);
-                        cards = newCards;
-                        return Rank.Five;
-                    }
-                    else
-                    {
-                        SetEveryJTo(newCards, threeCount);
-                        cards = newCards;
-                        return Rank.Five;
-                    }
+                    return Rank.Five;
                 }
 
                 if(curr == Rank.Two)
@@ -268,71 +182,25 @@ namespace AdventOfCode.Days
                     Card pair1 = CardCounts.First(x => x.Value == 2).Key;
                     Card pair2 = CardCounts.Last(x => x.Value == 2).Key;
 
-                    if(pair1 == Card.J)
+                    // J J 1 1 3 => 1 1 1 1 3
+                    if(pair1 == Card.J || pair2 == Card.J)
                     {
-                        SetEveryJTo(newCards, pair2);
-                        cards = newCards;
                         return Rank.Four;
                     }
 
-                    if(pair2 == Card.J)
-                    {
-                        SetEveryJTo(newCards, pair1);
-                        cards = newCards;
-                        return Rank.Four;
-                    }
-
-                    SetEveryJTo(newCards, HighestCard(pair1, pair2));
-                    cards = newCards;
-                    return Rank.Full; // 3 or full 
+                    // 2 2 1 1 J => 2 2 2 1 1
+                    return Rank.Full;
                 }
 
+                // J J 1 2 3 => 1 1 1 2 3
+                // 1 1 J 2 3 => 1 1 1 2 3
                 if(curr == Rank.One)
                 {
-                    Card pair1 = CardCounts.First(x => x.Value == 2).Key;
-                    List<Card> others = new();
-                    for(int i = 0; i < Cards.Length; ++i)
-                    {
-                        if (newCards[i] != pair1)
-                            others.Add(newCards[i]);
-                    }
-
-                    if(pair1 == Card.J)
-                    {
-                        SetEveryJTo(newCards, HighestCard(others.ToArray()));
-                        cards = newCards;
-                        return Rank.Three;
-                    }
-                    else
-                    {
-                        SetEveryJTo(newCards, pair1);
-                        cards = newCards;
-                        return Rank.Three;
-                    }
+                    return Rank.Three;
                 }
 
-                List<Card> notJ = new();
-                for(int i = 0; i < Cards.Length; ++i)
-                {
-                    if (Cards[i] != Card.J)
-                        notJ.Add(Cards[i]);
-                }
-                SetEveryJTo(newCards, HighestCard(notJ.ToArray()));
-                cards = newCards;
+                // J 1 2 3 4 => 1 1 2 3 4
                 return Rank.One;
-            }
-
-            public Card Highest()
-            {
-                Card high = Cards[0];
-
-                for(int i = 1; i < Cards.Length; ++i)
-                {
-                    if (Cards[i] > high)
-                        high = Cards[i];
-                }
-
-                return high;
             }
 
             public int CompareHighest(Hand other)
@@ -372,7 +240,6 @@ namespace AdventOfCode.Days
 
             public int CompareTo(Hand other)
             {
-                var me = Cards;
                 Rank mine = GetRank();
                 Rank otherR = other.GetRank();
 
@@ -384,8 +251,8 @@ namespace AdventOfCode.Days
 
             public int CompareToPart2(Hand other)
             {
-                Rank mine = IncreaseRank(out var myIncrease);
-                Rank otherR = other.IncreaseRank(out var otherIncrease);
+                Rank mine = IncreaseRank();
+                Rank otherR = other.IncreaseRank();
 
                 if (mine == otherR)
                     return CompareHighestPart2(other);
