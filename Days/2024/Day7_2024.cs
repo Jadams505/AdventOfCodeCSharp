71fbe418beace9a4d317d857858d458537127f3e
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2024-12-07 13:04:00 -0500

    clean up day7

--- Days/2024/Day7_2024.cs
+++ Days/2024/Day7_2024.cs
@@ -9,7 +9,7 @@ internal class Day7_2024 : Day2024
 {
     public override Regex ParseString => throw new NotImplementedException();
 
-    public List<(long, List<long>)> Equations { get; } = [];
+    public List<(long Result, List<long> Arguments)> Equations { get; } = [];
 
     public override void ConvertData()
     {
@@ -25,11 +25,9 @@ internal class Day7_2024 : Day2024
         }
     }
 
-    public List<(Operation Opp, long Num)> SolveCache = [];
-
     public bool Eval(long test, long val, int index, List<long> equation)
     {
-        if (test == val && index == equation.Count /*This was my bug*/)
+        if (test == val && index == equation.Count)
         {
             return true;
         }
@@ -49,13 +47,11 @@ internal class Day7_2024 : Day2024
 
         if (Eval(test, val * equation[index], index + 1, equation))
         {
-            SolveCache.Add((Operation.Mult, equation[index]));
             return true;
         }
             
         if (Eval(test, val + equation[index], index + 1, equation))
         {
-            SolveCache.Add((Operation.Add, equation[index]));
             return true;
         }
         return false;
@@ -63,7 +59,7 @@ internal class Day7_2024 : Day2024
 
     public bool Eval2(long test, long val, int index, List<long> equation)
     {
-        if (test == val && index == equation.Count /*This was my bug*/)
+        if (test == val && index == equation.Count)
         {
             return true;
         }
@@ -82,112 +78,44 @@ internal class Day7_2024 : Day2024
 
 
         if (Eval2(test, val * equation[index], index + 1, equation))
-        {
-            SolveCache.Add((Operation.Mult, equation[index]));
             return true;
-        }
 
         if (Eval2(test, val + equation[index], index + 1, equation))
-        {
-            SolveCache.Add((Operation.Add, equation[index]));
             return true;
-        }
 
-        // super dumb way to concat, but whatever
-        if (Eval2(test, long.Parse($"{val}{equation[index]}"), index + 1, equation))
-        {
-            SolveCache.Add((Operation.Concat, equation[index]));
+        // ignore negatives
+        var digits = equation[index] == 0
+            ? 1
+            : (long)Math.Log10(equation[index]) + 1;
+        if (Eval2(test, val * (long)Math.Pow(10, digits) + equation[index], index + 1, equation)) 
             return true;
-        }
 
         return false;
-    }
-
-    public enum Operation
-    {
-        Add,
-        Mult,
-        Concat,
-        None
-    };
-
-    public List<List<(Operation Opp, long Num)>> Solutions = [];
-
-    public long GetSolution(List<(Operation Opp, long Num)> solution)
-    {
-        long total = 0;
-        foreach (var (Opp, Num) in solution)
-        {
-            if (Opp is Operation.None) total = Num;
-            if (Opp is Operation.Add) total += Num;
-            if (Opp is Operation.Mult) total *= Num;
-        }
-        return total;
-    }
-
-    public void PrintSolution(long answer, List<(Operation Opp, long Num)> solution)
-    {
-        Console.Write($"{answer} = ");
-        foreach(var (Opp, Num) in solution)
-        {
-            if (Opp is Operation.None) Console.Write(Num);
-            if (Opp is Operation.Add) Console.Write($" + {Num}");
-            if (Opp is Operation.Mult) Console.Write($" * {Num}");
-        }
-        Console.WriteLine();
     }
 
     public override long GetSolution1()
     {
         long sum = 0;
-        int count = 0;
         foreach (var equation in Equations)
         {
-            count++;
-            SolveCache = [];
-            if (Eval(equation.Item1, equation.Item2[0], 1, equation.Item2))
+            if (Eval(equation.Result, equation.Arguments[0], 1, equation.Arguments))
             {
-                SolveCache.Reverse(); // since the recursion is backwards
-                sum += equation.Item1;
-                var sol = SolveCache.Prepend((Operation.None, equation.Item2[0])).ToList();
-                var answer = GetSolution(sol);
-                //PrintSolution(equation.Item1, sol);
-                Solutions.Add(sol);
+                sum += equation.Result;
             }
         }
-
-        // 304936744902 too high
-        // 303943215325 too high
-
         return sum;
     }
 
     public override long GetSolution2()
     {
         long sum = 0;
-        int count = 0;
         foreach (var equation in Equations)
         {
-            count++;
-            SolveCache = [];
-            if (Eval2(equation.Item1, equation.Item2[0], 1, equation.Item2))
+            if (Eval2(equation.Result, equation.Arguments[0], 1, equation.Arguments))
             {
-                SolveCache.Reverse(); // since the recursion is backwards
-                sum += equation.Item1;
-                var sol = SolveCache.Prepend((Operation.None, equation.Item2[0])).ToList();
-                var answer = GetSolution(sol);
-                if (sol.Count != equation.Item2.Count)
-                {
-                    int x = 3;
-                }
-                //PrintSolution(equation.Item1, sol);
-                Solutions.Add(sol);
+                sum += equation.Result;
             }
         }
-
-        // 1035826007640 too low
-        // 2165139500917 too low
-
         return sum;
     }
 }
